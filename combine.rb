cf370045b7d4f6d0c689905dce3cd78cbe52c089
Ryan <ryan@ryanjm.com>
2013-04-11 12:07:47 -0600

    reversed order of images

--- combine.rb
+++ combine.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 
-if ARGV.length < 2
+if ARGV.length <= 2
   puts "need more arguments"
 else
-  puts ARGV
-  first, last = ARGV.shift, "/Users/ryanjm/Desktop/#{ARGV.last}.png"
-  while (ARGV.size > 1)
-    second = ARGV.shift
-    puts "+append #{first} #{second} #{last}"
+  # reverse order or images to show up in order they were taken
+  args = ARGV.reverse
+  # last argument was random name for new image
+  last, first = "/Users/ryanjm/Desktop/#{args.shift}.png", args.shift
+  while (args.size > 0)
+    second = args.shift
     %x(convert +append '#{first}' '#{second}' '#{last}')
     first = last
   end
